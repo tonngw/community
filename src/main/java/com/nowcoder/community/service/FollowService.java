2d13a9c9ed4233a84fcd68813fbd455e51f891a8
tonngw <1336572985@qq.com>
2022-01-16 22:49:59 +0800

    添加关注和取消关注功能

---
+++ src/main/java/com/nowcoder/community/service/FollowService.java
@@ -0,0 +1,107 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.util.RedisKeyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author tonngw
+ * @date 2022-01-16 21:36
+ */
+@Service
+public class FollowService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 关注
+     *
+     * @param userId     用户 id
+     * @param entityType 实体类型
+     * @param entityId   实体 id
+     */
+    public void follow(int userId, int entityType, int entityId) {
+        // 要完全两个业务需要 redis 事务保证
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+
+                operations.opsForZSet().add(followeeKey, entityId, System.currentTimeMillis());
+                operations.opsForZSet().add(followerKey, userId, System.currentTimeMillis());
+
+                return operations.exec();
+            }
+        });
+    }
+
+    /**
+     * 取消关注
+     *
+     * @param userId     用户 id
+     * @param entityType 实体类型
+     * @param entityId   实体 id
+     */
+    public void unfollow(int userId, int entityType, int entityId) {
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+                String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+
+                operations.multi();
+
+                operations.opsForZSet().remove(followeeKey, entityId);
+                operations.opsForZSet().remove(followerKey, userId);
+
+                return operations.exec();
+            }
+        });
+    }
+
+    /**
+     * 查询关注的实体的数量
+     *
+     * @param userId     用户 id
+     * @param entityType 实体类型
+     * @return 关注数量
+     */
+    public long findFolloweeCount(int userId, int entityType) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().zCard(followeeKey);
+    }
+
+    /**
+     * 查询实体的粉丝的数量
+     *
+     * @param entityType 实体类型
+     * @param entityId   实体 id
+     * @return 粉丝数量
+     */
+    public long findFollowerCount(int entityType, int entityId) {
+        String followerKey = RedisKeyUtil.getFollowerKey(entityType, entityId);
+        return redisTemplate.opsForZSet().zCard(followerKey);
+    }
+
+    /**
+     * 查询当前用户是否已关注该实体
+     * @param userId     用户 id
+     * @param entityType 实体类型
+     * @param entityId   实体 id
+     * @return true or false
+     */
+    public boolean hasFollowed(int userId, int entityType, int entityId) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
+        return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+}
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -10,10 +10,33 @@ public class RedisKeyUtil {
     
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    private static final String PREFIX_USER_LIKE = "like:user";
+    // followee 被关注者
+    private static final String PREFIX_FOLLOWEE = "followee";
+    // follower 关注者
+    private static final String PREFIX_FOLLOWER = "follower";
     
     // 某个实体的赞（帖子、评论）
     // like:entity:entityType:entityId -> set(userId)
     public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    // 某个用户的赞
+    // like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+    
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWEE + SPLIT + entityType + SPLIT + entityId;
     }
 }
